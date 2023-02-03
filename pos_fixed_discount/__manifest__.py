cc257201cabd9bc839b2f92bf54a37f1f1a58b2c
Mohammed Alwasefy <mohammed.alwasefy@gmail.com>
2023-02-03 21:03:39 +0200

    Add files via upload

---
+++ pos_fixed_discount/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "POS Fixed Discount",
+    "summary": "User able to add fixed discount to the pos order line.",
+    'author': "Mohammed Alwasefy",
+    'maintainer': 'Mohammed Alwasefy(mohammed.alwasefy@gmail.com)',
+    "license": "AGPL-3",
+    'category': 'Point Of Sale',
+    'version': '15.0.1',
+    "depends": ["point_of_sale"],
+    "application": True,
+    "data": [
+             'views/pos_config.xml',
+            ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_fixed_discount/static/src/js/order_line_fixed_discount.js',
+        ],
+        'web.assets_qweb': [
+            'pos_fixed_discount/static/src/xml/pos.xml'
+        ]
+    },
+}
