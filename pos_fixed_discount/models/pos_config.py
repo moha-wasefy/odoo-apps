61c9200a050efc9b6d3e0c3270b7e603f0cabb33
Mohammed Alwasefy <mohammed.alwasefy@gmail.com>
2023-02-03 21:20:34 +0200

    Delete pos_fixed_discount directory

--- pos_fixed_discount/models/pos_config.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class PosConfig(models.Model):
-    _inherit = 'pos.config'
-
-    enable_fixed_discount = fields.Boolean(
-        "Active Fixed Discount",)
